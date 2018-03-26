b43ee06beb67d203546c5f083cd34a1b713fdacc
Jason Crawford <daxapps777@gmail.com>
2018-03-26 16:29:32 -0700

    Toggle register form

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -7,18 +7,23 @@ export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
   userToken: any;
 
-constructor(private http: Http) { }
+  constructor(private http: Http) {}
 
-login(model: any) {
-  const headers = new Headers({'Content-type': 'application/json'});
-  const options = new RequestOptions({headers: headers});
-  return this.http.post(this.baseUrl + 'login', model, options).map((response: Response) => {
-      const user = response.json();
-      if (user) {
-          localStorage.setItem('token', user.tokenString);
-          this.userToken = user.tokenString;
-      }
-  });
-}
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model, this.requestOptions()).map((response: Response) => {
+        const user = response.json();
+        if (user && user.tokenString) {
+            localStorage.setItem('token', user.tokenString);
+        }
+    });
+  }
 
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+  }
+
+  private requestOptions() {
+    const headers = new Headers({ 'Content-type': 'application/json' });
+    return new RequestOptions({ headers: headers });
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,18 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   model: any = {};
+  @Output() cancelRegister = new EventEmitter();
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      console.log('Registration successful');
+    }, error => {
+      console.log(error);
+    });
   }
 
   cancel() {
-    console.log('cancelled');
+    this.cancelRegister.emit(false);
   }
 
 }
